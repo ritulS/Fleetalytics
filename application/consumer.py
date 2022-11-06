edd94d7c03802d0795bd5524e5872a32e8754283
Ritul <rituls619@gmail.com>
2022-11-06 15:15:51 +0530

    ~

--- application/consumer.py
+++ application/consumer.py
@@ -1,15 +1,32 @@
 import time
 import pika
+import sys, os
+import mysql.connector
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
 
-channel.queue_declare('telematics')
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    
-channel.basic_consume(queue='hello',
-                      auto_ack=True,
-                      on_message_callback=callback)
+    channel.queue_declare(queue='telematics')
 
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+    channel.basic_consume(queue='hello',
+                        auto_ack=True,
+                        on_message_callback=callback)
+
+    print('Waiting for messages')
+
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os.exit(0)
