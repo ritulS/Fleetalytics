fce99fcb1762af01e076becf5c77153a903bc7f0
AlfredDaimari <alfreddaimari77@gmail.com>
2022-11-16 00:50:53 +0530

    feature - added cache
    - now backend returns latest cache update using car id

--- nodeserv/backend.ts
+++ nodeserv/backend.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import FuzzySearch from "fuzzy-search";
 import cars_json from "./cars.json";
+import { createClient } from "redis";
 
 // backed to send car data to the frontend
 
@@ -11,13 +12,34 @@ const searcher = new FuzzySearch(CARS, ["id"], {
   sort: true,
 });
 
+// setting up redis connection
+const client = createClient({
+  socket: {
+    host: "127.0.0.1",
+    port: 6379,
+  },
+  password: "hello",
+});
+
+client.connect();
+client.on("connect", () =>
+  console.log("client is now connected to redis cache!")
+);
+client.on("error", (err) => console.log("error" + err));
+
+// setting up express server
 const app: Express = express();
 
 app.use(express.static("public"));
 
-app.get("/car_status/:carID", (req: Request, res: Response) => {
-  const reqCarID: String = req.params["carID"];
-  res.send(`${reqCarID} request was received at the backend`);
+app.get("/car_status/:carID", async (req: Request, res: Response) => {
+  const reqCarID: string = req.params["carID"];
+  try {
+    const value = await client.get(reqCarID);
+    res.send(value);
+  } catch {
+    res.json({ err: "car with id not found" });
+  }
 });
 
 // fuzzy searching for the car id
