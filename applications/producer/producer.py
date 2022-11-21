f161bb5bc5c124c40ca79639ad68a39b4e8ecf27
Ritul <rituls619@gmail.com>
2022-11-21 22:43:55 +0530

    ~

--- applications/producer/producer.py
+++ applications/producer/producer.py
@@ -1,16 +1,19 @@
-import time
-import json
-import pika
+# import time
+# import json
+# import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+# channel = connection.channel()
 
-channel.queue_declare(queue='telematics')
+# channel.queue_declare(queue='telematics')
 
-channel.basic_publish(exchange='',
-                        routing_key='telematics',
-                        body='Vin:1234')
+# channel.basic_publish(exchange='',
+#                         routing_key='telematics',
+#                         body='Vin:1234')
 
-print("Vin number sent")
-connection.close()
+# print("Vin number sent")
+# connection.close()
+
+
+
 
