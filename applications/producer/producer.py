e5ccc0f38546f4177f79661b5ca2590a435030ab
Ritul <rituls619@gmail.com>
2022-11-22 17:21:24 +0530

    producer query setup with db1

--- applications/producer/producer.py
+++ applications/producer/producer.py
@@ -14,6 +14,32 @@
 # print("Vin number sent")
 # connection.close()
 
+import psycopg2
 
+# Connect with local postgres DB
+try: 
+    print('before conn')
+    conn = psycopg2.connect("dbname='bus_data' user='postgres' password='postgres' host='localhost'")
+    print('after conn')
+    cursor = conn.cursor()
+    psql_select_query = "SELECT * FROM bus_table"
+
+    cursor.execute(psql_select_query)
+    print("Selecting all rows in bus_table")
+
+    bus_records = cursor.fetchall()
+    i = 0
+    for row in bus_records:
+        i += 1
+        print(row)
+        if i > 5:
+            break
+    
+    
+
+
+except (Exception, psycopg2.Error) as error:
+    print("Error while fetching data from DB 1")
+    print(error)
 
 
