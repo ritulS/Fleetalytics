412485f9d91c7005ebb4498cd3c58bd3da2eae25
Ritul <rituls619@gmail.com>
2022-11-29 11:58:46 +0530

    ~

--- producer/producer.py
+++ producer/producer.py
@@ -4,7 +4,7 @@ import time
 import json
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='13.234.117.189'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
