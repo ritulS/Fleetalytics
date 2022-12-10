f9e176e30e694e508b0944c0cb743ef678d92a2f
Ritul <rituls619@gmail.com>
2022-12-10 17:54:48 +0530

    ~

--- producer/producer.py
+++ producer/producer.py
@@ -3,12 +3,13 @@
 import time
 import json
 import pika
+from datetime import datetime
+import threading
+# connection = pika.BlockingConnection(
+#     pika.ConnectionParameters(host='172.26.1.243', port=5672, socket_timeout=2))
+# channel = connection.channel()
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='172.26.1.243', port=5672, socket_timeout=2))
-channel = connection.channel()
-
-channel.exchange_declare(exchange='logs', exchange_type='fanout')
+# channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
 # print("Vin number sent")
 
@@ -18,24 +19,46 @@ import psycopg2
 try:
     #print('before conn')
     conn = psycopg2.connect(
-        "dbname='bus_data' user='postgres' password='postgres' host='localhost'"
+        "dbname='db1' user='postgres' password='postgres' host='localhost'"
     )
-    #print('after conn')
+    print('Connected to DB 1')
     cursor = conn.cursor()
-    psql_select_query = "SELECT * FROM bus_table"
+    cursor.execute("""
+    SELECT * from bus_data where sr_num=1;
+    """)
+    print(cursor.fetchall())
 
-    cursor.execute(psql_select_query)
-    print("Selecting all rows in bus_table")
+    
+    time_now = datetime.now().time() # time object
 
-    bus_records = cursor.fetchall()
-    i = 0
+    # threading to query every n seconds
+    sn = 0
+    def query_n_secs(sn,cursor):
+        #threading.Timer(4.0,query_n_secs).start()
+        sn += 4
+        cursor.execute("""
+        "Select * from bus_data where sr_num=%(int)s"
+        VALUES (%(int)s, %(date)s, %(date)s, %(str)s);
+        """,
+        {'int': sn})
+        print(cursor.fetchall())
+        time.sleep(3)
+            
+    query_n_secs(0,cursor)
+    # psql_select_query = "Select * from bus_data where sr_num=1"
 
-    for row in bus_records:
-        i += 1
-        message = json.dumps(row, default=str)
-        channel.basic_publish(exchange='logs', routing_key='', body=message)
+    # cursor.execute(psql_select_query)
+    # print("Selecting all rows in bus_table")
 
-    connection.close()
+    # bus_records = cursor.fetchall()
+    # i = 0
+
+    # for row in bus_records:
+    #     i += 1
+    #     message = json.dumps(row, default=str)
+    #     channel.basic_publish(exchange='logs', routing_key='', body=message)
+
+    # connection.close()
 
 except (Exception, psycopg2.Error) as error:
     print(error)
