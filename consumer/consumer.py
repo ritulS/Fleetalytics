e2a8c5176268de0002565cea583d98563ebcce0e
Ritul <rituls619@gmail.com>
2022-12-10 12:52:55 +0530

    ~

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,5 +1,6 @@
 import pika
 import redis
+import json
 #import mysql.connector
 
 REDIS: redis.Redis
@@ -10,7 +11,22 @@ def connect_to_redis_cache(host: str, port: int, password: str):
     REDIS = redis.Redis(host=host, port=port, password=password)
 
 
+def get_from_cache(id: str):
+    '''
+    Get value from cache 
+    - should be in JSON format
+    '''
+    global REDIS
+    return REDIS.get(id)
+
+
 def push_to_cache(id: str, value: str):
+    '''
+    Push value to cache 
+        Parameters:
+            id: DUH!?
+            value: must be in JSON format
+    '''
     global REDIS
     REDIS.set(id, value)
     print(f"set value {id} with {value}")
@@ -20,17 +36,42 @@ def push_to_db(data):
     pass
 
 
+def calculate_distance(cur_data: str) -> int:
+    '''
+    Calculate the distance using coordinates
+    '''
+    return 0
+
+
 def rbmq_callback(ch, method, properties, body: str):
-    """
+    '''
     Callback function for rabbitmq
 
     body:str - sent in the format "id:'{}'" where '{}' is in JSON format
-    """
+    ["2019-01-25", "08:50:39", "C51623", 371.0, "-22.883270", "-43.342560", 37.0]
+
+    Cache format
+
+    carStatus:{
+        cid: cid as string,
+        fuel: 0,
+        speed: 0,
+        location: [0, 0] as [number, number],
+        avg_distance: 0,
+        avg_speed: 0,
+    },
+    '''
     print(" [x] Received %r" % body)
     body_arr = body.split(':')
     id = body_arr[0]
     value = body_arr[1]
-    #push_to_cache(id, value)
+    # convert json to py list
+    data = json.loads(body)
+
+    # call distance function
+
+    #push_to_db(data)
+    push_to_cache(id, value)
 
 
 def connect_to_rbmq_broker(ip: str):
@@ -59,9 +100,8 @@ def connect_to_rbmq_broker(ip: str):
 
 
 def main():
-    #TODO - write host, port, password
-    #connect_to_redis_cache()
-    connect_to_rbmq_broker("ip-172-26-1-243")
+    connect_to_redis_cache(host="redis.cache", port=6379, password="hello")
+    connect_to_rbmq_broker("rabbitmq3")
 
 
 if __name__ == '__main__':
