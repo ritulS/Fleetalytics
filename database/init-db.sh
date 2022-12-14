1d00f2402336a3ec069d9c7bee2050d1e13beb4b
AlfredDaimari <alfred_daimari@yahoo.com>
2022-12-14 11:00:20 +0530

    postgres - fix (create_database)

--- database/init-db.sh
+++ database/init-db.sh
@@ -2,8 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER log;
-	CREATE DATABASE carlogs;
-	GRANT ALL PRIVILEGES ON DATABASE carlogs TO log;
-    CREATE TABLE bus_data(sr_num INT NOT NULL, date0 CHAR(50) NOT NULL, time0 CHAR(30) NOT NULL, vin CHAR(10) NOT NULL, route INT NOT NULL, latitude FLOAT, longitude FLOAT, speed FLOAT, type CHAR(5) NOT NULL, date DATE NOT NULL, time TIME NOT NULL);
+        SELECT 'CREATE DATABASE db2' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'db2');
+	CREATE TABLE bus_data(sr_num INT NOT NULL, date0 CHAR(50) NOT NULL, time0 CHAR(30) NOT NULL, vin CHAR(10) NOT NULL, route INT NOT NULL, latitude FLOAT, longitude FLOAT, speed FLOAT, type CHAR(5) NOT NULL, date DATE NOT NULL, time TIME NOT NULL);
 EOSQL
