bd86e3697bc58cc7519d4a2aa563b4051fa9e4d8
AlfredDaimari <alfred_daimari@yahoo.com>
2022-12-14 13:54:02 +0530

    fix - log feature

--- database/init-db.sh
+++ database/init-db.sh
@@ -3,7 +3,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "$POSTGRES_DB" <<-EOSQL
         SELECT 'CREATE DATABASE db2' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'db2');
-	CREATE TABLE bus_data(sr_num INT NOT NULL, date0 CHAR(50) NOT NULL, time0 CHAR(30) NOT NULL, vin CHAR(10) NOT NULL, route INT NOT NULL, latitude FLOAT, longitude FLOAT, speed FLOAT, type CHAR(5) NOT NULL, date DATE NOT NULL, time TIME NOT NULL);
+	CREATE TABLE bus_data(sr_num INT NOT NULL, date0 CHAR(50) NOT NULL, time0 CHAR(30) NOT NULL, vin CHAR(10) NOT NULL, route INT NOT NULL, latitude FLOAT, longitude FLOAT, speed FLOAT, type CHAR(5) NOT NULL, date DATE NOT NULL, time TIME NOT NULL, delta_d FLOAT NOT NULL);
 EOSQL
 
 
